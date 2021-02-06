45dd6a825ef4d1a4c5d2869a01c45604b61a50dc
mgoral2 <mgoralka4@gmail.com>
2021-02-06 17:57:46 -0600

    got like and nope working

--- ProfilePic.js
+++ ProfilePic.js
@@ -2,6 +2,8 @@
 import * as React from "react";
 import {Image, StyleSheet, View, Text, Dimensions} from "react-native";
 
+import Animated from "react-native-reanimated";
+
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -9,6 +11,7 @@ var image1 = require('./images/1.jpg');
 
 export default function ProfilePic() {
   return (
+    <View >
     <View>
       <Image source ={image1}
       style={{
@@ -17,5 +20,38 @@ export default function ProfilePic() {
         resizeMode: 'contain'
       }}/>
     </View>
+
+    <Animated.View style={{
+      position: 'absolute',
+      top: windowHeight*.1,
+      left: windowWidth*.1,
+    }}
+    >
+    <Text style = {styles.likeLabel}>Like</Text>
+    </Animated.View>
+
+    <Animated.View style={{
+      position: 'absolute',
+      top: windowHeight*.1,
+      right: windowWidth*.1,
+
+    }}
+    >
+    <Text style = {styles.nopeLabel}>Nope</Text>
+    </Animated.View>
+    </View>
   )
 };
+
+const styles = StyleSheet.create({
+  likeLabel: {
+    fontSize: 32,
+    color: "#6ee3b4",
+    fontWeight: "bold",
+  },
+  nopeLabel: {
+    fontSize: 32,
+    color: "#ec5288",
+    fontWeight: "bold",
+  },
+})
