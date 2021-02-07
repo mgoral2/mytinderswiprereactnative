d2e1e9ef6c7d895912d7d94131077c0ffd1a9809
mgoral2 <mgoralka4@gmail.com>
2021-02-07 10:59:12 -0600

    some errors, pushing before changing

--- App.js
+++ App.js
@@ -15,7 +15,41 @@ const LikeImage = require('./images/like.png');
 
 const {
   event, Value, interpolate, concat, Extrapolate, cond, eq, set,
+  clockRunning, startClock, stopClock, spring, Clock, greaterThan, lessThan,
+  and,
 } = Animated;
+
+function runSpring(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    velocity: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+  };
+
+  const config = {
+    damping: 20,
+    mass: 1,
+    stiffness: 100,
+    overshootClamping: false,
+    restSpeedThreshold: 1,
+    restDisplacementThreshold: 0.5,
+    toValue: new Value(0),
+  };
+
+  return [
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.velocity, 0),
+      set(state.position, value),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    spring(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+    state.position,
+  ];
+}
 
 export default class App extends Component {
   constructor(props: ProfilesProps) {
@@ -43,16 +77,41 @@ export default class App extends Component {
 }
 
 init() {
+
+  const {
+    gestureState, translationX, translationY, velocityX,
+  } = this;
+
+  const clockX = new Clock();
+  const clockY = new Clock();
+  const snapPoint = cond(and(lessThan(translationX, 0), lessThan(velocityX, -10))
+  -windowWidth,
+  cond(
+    and(greaterThan(translationX, 0), greaterThan(velocityX, 10)),
+    windowWidth,
+    0,
+
+  )
+)
+
   this.translateX = cond(eq(gestureState, State.END), [
-    set(translationX, runSpring()),
-    translationX
+    set(translationX, runSpring(clockX, translationX, velocityX, snapPoint)),
+    translationX,
   ],
   translationX
-)
+);
+
+this.translateY = cond(eq(gestureState, State.END), [
+  set(translationY, runSpring(clockY, translationY, 0, 0)),
+  translationY,
+],
+translationY
+);
+
 }
 
 render() {
-  const {onGestureEvent, translationX: translateX, translationY: translateY } = this;
+  const {onGestureEvent, translateX, translateY } = this;
 
   const rotateZ = concat(
     interpolate(translateX, {
